f9b96d262a232a234b1b53a703215772a0cf30a9
dfsaigua <dfsaigua@espol.edu.ec>
2017-10-12 12:33:59 -0500

    programa de saludo

---
+++ saludos.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class saludos {
+ 
+    
+    
+    public static void main (String [ ] args) {
+                
+                String n ="";
+                System.out.println("Ingrese su nombre");        
+                 Scanner entradaEscaner = new Scanner (System.in);
+                 n = entradaEscaner.nextLine ();
+                  System.out.println("Saludos" + n);  
+        }
+        
+    
+    
+}
